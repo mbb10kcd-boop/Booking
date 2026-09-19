a47368176f8546eee5b0abd39fef957697e482db
mbb10kcd-boop <mbb10kcd@gmail.com>
2026-09-19 21:55:46 +0200

    Delete route (3).ts

--- route (3).ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-import { db, schema } from "@/db";
-import { desc } from "drizzle-orm";
-
-/** Viser genererede beskeder (simuleret afsendelse - der er endnu ikke koblet en rigtig mailudbyder på) */
-export async function GET() {
-  const log = await db.select().from(schema.notificationLog).orderBy(desc(schema.notificationLog.sentAt));
-  return NextResponse.json(log);
-}
